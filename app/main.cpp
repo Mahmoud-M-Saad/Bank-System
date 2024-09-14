9a8a122da6074576102fa0f65e2fbbf56cda573d
Mahmoud Mohamed <mahmoudsaad6675@gmail.com>
2024-09-14 14:17:55 +0300

    JSON files code, strating with employee.

--- app/main.cpp
+++ app/main.cpp
@@ -3,7 +3,14 @@
 using namespace std;
 
 int main() {
+	//! Get All Data from JSON Files
+	loadEmpsFromJson();
+	//! ============================
 
-	cout << "Welcome, Sir! This project is working great.";
+	//! Project Logic
+	cout << "Welcome, Sir! This project is working great."<< endl;
+	printAllEmp();
 
+	//! Saving All Data to JSON Files
+	saveEmpsToJson();
 }
